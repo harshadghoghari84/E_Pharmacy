a3a4345c9691a322ec51e65953ff8b69cfcdccf0
Harshad <harshad@192.168.1.9>
2023-02-21 22:19:27 +0530

    undo add constatnt url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,6 +2,8 @@ export default {
   loggedUserData :"userData",
   prfUserToken: "userToken",
   serverUrl: "http://localhost:4001/graphql",
+  // serverUrl: process.env.REACT_APP_SERVER_URL,
   badgeSucessType: "active-badge",
   badgeDangerType: "danger-badge",
 };
+
