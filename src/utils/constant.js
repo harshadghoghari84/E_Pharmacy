64ceb7392423c38cfb7e3d6c2c61a180da7bbdda
Harshad <harshad@192.168.1.10>
2023-03-01 21:22:56 +0530

    make a build

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,7 @@
 export default {
   loggedUserData :"userData",
   prfUserToken: "userToken",
-  serverUrl: "http://localhost:4001/graphql",
+  serverUrl: "http://api.medicinezilla.com/graphql",
   // serverUrl: process.env.REACT_APP_SERVER_URL,
   badgeSucessType: "active-badge",
   badgeDangerType: "danger-badge",
